0d910d33f7148cfdf8ac7e6c69ed50c32a2c77ce
Darshan M <projectsfordmj@gmail.com>
2021-07-05 12:25:37 +0530

    manage account nos

--- createAccount.c
+++ createAccount.c
@@ -1,101 +1,124 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "header.h"
+
+void createDB();
+
+char* accountNo;
+char* date;
+long accountN;
+
 void createAccount(struct accountdetail *account)
 {
-    FILE *fp;
-    fp=fopen("AccountDetails.csv","a++");
+    FILE *fp, *global;
+    char buffer[1024];
 
-   printf("Account Holder Name : ");
-   fscanf(stdin,"%s",account->name)!=EOF;
-   fprintf(fp,"\n%s,",account->name);
-   
-   printf("\ndd/mm/yyyy : ");
-   fscanf(stdin,"%d %d %d",&account->date.dd,&account->date.mm,&account->date.yy);
-   fprintf(fp,"%d / %d / %d ,",account->date.dd,account->date.mm,account->date.yy);
-    
-   printf("\nGender : ");
-   fscanf(stdin,"%s",account->gender);
-   fprintf(fp,"%s,",account->gender);
-    
+    fp = fopen("D:\\DARSHAN BTECH\\bank-cMiniProject\\database\\AccountDetails.csv", "a++");
+
+    global = fopen("D:\\DARSHAN BTECH\\bank-cMiniProject\\database\\global.csv", "r");
+
+    fgets(buffer, 1024, global);
+
+    accountNo = strtok(buffer, ",");
+    date = strtok(NULL, ",");
+
+    printf("%s", accountNo);
+    printf("%s", date);
+
+    printf("Account Holder Name : ");
+    fscanf(stdin, "%s", account->name) != EOF;
+    fprintf(fp, "\n%s,", account->name);
+    fflush(stdin);
+
+    printf("\ndd/mm/yyyy : ");
+    fscanf(stdin, "%d %d %d", &account->date.dd, &account->date.mm, &account->date.yy);
+    fprintf(fp, "%d / %d / %d ,", account->date.dd, account->date.mm, account->date.yy);
+    fflush(stdin);
+
+    printf("\nGender : ");
+    fscanf(stdin, "%s", account->gender);
+    fprintf(fp, "%s,", account->gender);
+    fflush(stdin);
+
     printf("\nOccupation : ");
-    fscanf(stdin,"%s,",account->occupation);
-    fprintf(fp,"%s,",account->occupation);
-     
+    fscanf(stdin, "%s,", account->occupation);
+    fprintf(fp, "%s,", account->occupation);
+    fflush(stdin);
+
     printf("\nNationality : ");
-    fscanf(stdin, "%s,",account->nationality);
-    fprintf(fp,"%s,",account->nationality);
+    fscanf(stdin, "%s,", account->nationality);
+    fprintf(fp, "%s,", account->nationality);
+    fflush(stdin);
 
     printf("\nAddress : ");
-    fscanf(stdin,"%s,",account->address);
-    fprintf(fp,"%s,",account->address);
+    fscanf(stdin, "%s,", account->address);
+    fprintf(fp, "%s,", account->address);
+    fflush(stdin);
 
     printf("\nMother name : ");
-    fscanf(stdin,"%s,",account->mname);
-    fprintf(fp,"%s,",account->mname);
-    
+    fscanf(stdin, "%s,", account->mname);
+    fprintf(fp, "%s,", account->mname);
+    fflush(stdin);
+
     printf("\nFather name : ");
-    fscanf(stdin,"%s,",account->fname);
-    fprintf(fp,"%s,",account->fname);
+    fscanf(stdin, "%s,", account->fname);
+    fprintf(fp, "%s,", account->fname);
+    fflush(stdin);
 
     printf("\nNominee Name : ");
-    fscanf(stdin,"%s,",account->nominee);
-    fprintf(fp,"%s,",account->nominee);
+    fscanf(stdin, "%s,", account->nominee);
+    fprintf(fp, "%s,", account->nominee);
+    fflush(stdin);
 
-    printf("\nAdhar Number : ");
-    fscanf(stdin,"%s,",account->adharno);
-    fprintf(fp,"%s,",account->adharno);
-    
+    printf("\nAadhar Number : ");
+    fscanf(stdin, "%s,", account->adharno);
+    fprintf(fp, "%s,", account->adharno);
+    fflush(stdin);
 
     printf("\nPAN Card Number : ");
-    fscanf(stdin,"%s,",account->pan);
-    fprintf(fp,"%s,",account->pan);
+    fscanf(stdin, "%s,", account->pan);
+    fprintf(fp, "%s,", account->pan);
+    fflush(stdin);
 
-    printf("Enter the amount u want to deposit : ");
-    fscanf(stdin,"%s",&account->amount);
-    fprintf(fp,"%s,",account->amount);
-    
+    printf("\nEnter the amount u want to deposit : ");
+    fscanf(stdin, "%s", &account->amount);
+    fprintf(fp, "%s,", account->amount);
+    fflush(stdin);
+
     printf("\nAccount Type\n");
     printf("1 . Savings Account\n");
     printf("2 . Current Account\n");
     printf("\n");
     printf("Enter the choice(Savings,Current) : ");
-    fscanf(stdin,"%s",account->acctype);
-    fprintf(fp,"%s",account->acctype);
+    fscanf(stdin, "%s", account->acctype);
+    fprintf(fp, "%s", account->acctype);
+
+   
+
+    createDB();
 
     printf("\nYour has been sucessfull created");
 
-    
+    fclose(fp);
+    fclose(global);
 }
 
 
+void createDB()
+{
+    FILE *global;
+    char* e;
 
+    global = fopen("D:\\DARSHAN BTECH\\bank-cMiniProject\\database\\global.csv", "w");
 
+    accountN = strtol(accountNo, &e, 10);
+    accountN++;
 
+    //Prajwal your code here.
+    //Use accountNo for string operations and accountN for integer operations
 
+    fprintf(global, "%ld,%s", accountN, date);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    
-    
-    
+    fclose(global);
+}
